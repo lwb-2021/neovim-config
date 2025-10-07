1cf6afaad35f7cb283e6047abc1d7b156a0b77fa
lwb <lwb-2021@qq.com>
2025-10-07 22:05:20 +0800

    apply config for vue-ls

--- lua/plugins/standalone/mason.lua
+++ lua/plugins/standalone/mason.lua
@@ -20,6 +20,47 @@ return {
       opts = {
       }
     },
-    "neovim/nvim-lspconfig",
+    {
+      "neovim/nvim-lspconfig",
+      config = function()
+        local mason_registry = require('mason-registry')
+        local vue_language_server_path = vim.fn.expand '$MASON/packages' ..
+            '/vue-language-server' .. '/node_modules/@vue/language-server'
+        local tsserver_filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' }
+        local vue_plugin = {
+          name = '@vue/typescript-plugin',
+          location = vue_language_server_path,
+          languages = { 'vue' },
+          configNamespace = 'typescript',
+        }
+        local vue_ls_config = {}
+
+        local vtsls_config = {
+          settings = {
+            vtsls = {
+              tsserver = {
+                globalPlugins = {
+                  vue_plugin,
+                },
+              },
+            },
+          },
+          filetypes = tsserver_filetypes,
+        }
+
+        local ts_ls_config = {
+          init_options = {
+            plugins = {
+              vue_plugin,
+            },
+          },
+          filetypes = tsserver_filetypes,
+        }
+        vim.lsp.config('vtsls', vtsls_config)
+        vim.lsp.config('vue_ls', vue_ls_config)
+        vim.lsp.config('ts_ls', ts_ls_config)
+        vim.lsp.enable({ 'vtsls', 'vue_ls' })
+      end
+    }
   },
 }
