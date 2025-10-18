96406619070fa7554fd6ead7cb71212d0890700a
lwb <lwb-2021@qq.com>
2025-10-18 16:12:32 +0800

    conform.nvim: enable lazy loading

--- lua/plugins/standalone/conform.lua
+++ lua/plugins/standalone/conform.lua
@@ -1,5 +1,18 @@
 return {
   "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      -- Customize or remove this keymap to your liking
+      "<leader>f",
+      function()
+        require("conform").format({ async = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
   opts = {
     format_on_save = {
       -- These options will be passed to conform.format()
