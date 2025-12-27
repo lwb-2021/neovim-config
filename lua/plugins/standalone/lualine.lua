22abd7a4c3c72bbebab0b5de51ba2d49fe01e89c
lwb <lwb-2021@qq.com>
2025-12-27 13:34:54 +0800

    lualine: add chars count

--- lua/plugins/standalone/lualine.lua
+++ lua/plugins/standalone/lualine.lua
@@ -1,10 +1,17 @@
+local function char_count()
+  return tostring(vim.fn.wordcount().chars)
+end
+
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
     options = {
       theme = "catppuccin",
-      component_separators = '';
-      section_separators = { left = '', right = ''},
+      component_separators = '',
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_z = { "location", char_count },
     }
   }
 }
