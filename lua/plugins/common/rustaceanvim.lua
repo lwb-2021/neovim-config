0a6ed39dcd63e4f21f3ab7bf306838577879ab6a
lwb <lwb-2021@qq.com>
2025-12-28 17:46:27 +0800

    rustaceanvim: change settings

--- lua/plugins/common/rustaceanvim.lua
+++ lua/plugins/common/rustaceanvim.lua
@@ -2,4 +2,23 @@ return {
   "mrcjkb/rustaceanvim",
   version = "^6", -- Recommended
   lazy = false,   -- This plugin is already lazy
+  opts = function(_, opts)
+    opts.server.default_settings["rust-analyzer"] = {
+      cargo = {
+        loadOutDirsFromCheck = true,
+        buildScripts = {
+          enable = true,
+        },
+      },
+      checkOnSave = true,
+      procMacro = {
+        enable = true,
+        ignored = {},
+      },
+      files = {
+        watcher = "client",
+        excludeDirs = { ".direnv", ".git" },
+      },
+    }
+  end,
 }
