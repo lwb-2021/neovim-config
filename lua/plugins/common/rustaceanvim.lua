aab54687c0c09caedcc06cbd923c96d589a44920
lwb <lwb-2021@qq.com>
2026-01-30 14:17:30 +0800

    rustaceanvim: fix

--- lua/plugins/common/rustaceanvim.lua
+++ lua/plugins/common/rustaceanvim.lua
@@ -3,21 +3,40 @@ return {
   version = "^6", -- Recommended
   lazy = false,   -- This plugin is already lazy
   config = function()
-    vim.g.rustaceanvim.server.default_settings["rust-analyzer"] = {
-      cargo = {
-        loadOutDirsFromCheck = true,
-        buildScripts = {
-          enable = true,
+    vim.g.rustaceanvim = {
+      server = {
+        auto_attach = function(bufnr)
+          vim.api.nvim_create_autocmd("User", {
+            pattern = { "DirenvReady", "DirenvNotFound" },
+            once = true,
+            callback = function()
+              if vim.bo.filetype == "rust" then
+                require("rustaceanvim.lsp").start(bufnr)
+              end
+            end,
+          })
+          return false
+        end,
+        default_settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
+              },
+            },
+            checkOnSave = true,
+            procMacro = {
+              enable = true,
+              ignored = {},
+            },
+            files = {
+              watcher = "client",
+              excludeDirs = { ".direnv", ".git" },
+            },
+          },
+
         },
-      },
-      checkOnSave = true,
-      procMacro = {
-        enable = true,
-        ignored = {},
-      },
-      files = {
-        watcher = "client",
-        excludeDirs = { ".direnv", ".git" },
       },
     }
   end,
