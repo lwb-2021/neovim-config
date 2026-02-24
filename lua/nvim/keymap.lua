dfd35e1bbb19964db7135ef2795a561c630aca1c
lwb <lwb-2021@qq.com>
2026-02-24 13:34:09 +0800

    keymap: add completion toggle

--- lua/nvim/keymap.lua
+++ lua/nvim/keymap.lua
@@ -3,4 +3,5 @@ local map = vim.keymap.set
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+map("n", "<leader>c", function() vim.b.completion = not vim.b.completion end, { desc = "Toggle completion" })
 -- map("n", "<leader>q", "bwipeout", { desc = "Close Buffer" })
