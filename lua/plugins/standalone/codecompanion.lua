d45c6752ec04c9e75c7c00043f17e341ce852070
lwb <lwb-2021@qq.com>
2026-03-07 20:48:09 +0800

    codecompanion: add siliconflow models

--- lua/plugins/standalone/codecompanion.lua
+++ lua/plugins/standalone/codecompanion.lua
@@ -1,11 +1,46 @@
+local siliconflow_api = {
+  api_key = "cmd:pass API/SiliconFlow/Completion",
+  url = "https://api.siliconflow.cn",
+}
+local siliconflow_schema = {
+  model = {
+    default = "Qwen/Qwen3.5-4B"
+  }
+}
+
 return {
   "olimorris/codecompanion.nvim",
   opts = {
-    strategies = {
-      inline = { adapter = "copilot" },
+    adapters = {
+      http = {
+        siliconflow = function()
+          return require("codecompanion.adapters").extend("openai_compatible", {
+            env = siliconflow_api,
+            schema = siliconflow_schema
+          })
+        end,
+      }
     },
+    strategies = {
+      inline = {
+        adapter = {
+          name = "siliconflow",
+          model = "Qwen/Qwen2.5-Coder-7B-Instruct"
+        }
+      },
+      chat = {
+        adapter = {
+          name = "siliconflow",
+          model = "Qwen/Qwen3.5-4B",
+        }
+      }
+    },
+    opts = {
+      -- language = "Chinese",
+    }
   },
   dependencies = {
     "nvim-lua/plenary.nvim",
+    -- "github/copilot.vim"
   },
 }
